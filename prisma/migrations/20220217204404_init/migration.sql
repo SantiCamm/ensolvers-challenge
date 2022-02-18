ed09810afe405341e7f6d56088417c6e5ae0bd7f
SantiCamm <santi.cammarano@gmail.com>
2022-02-18 15:45:22 -0300

    final changes

--- prisma/migrations/20220217204404_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Todo" DROP CONSTRAINT "Todo_folderId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Todo" ADD CONSTRAINT "Todo_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { googleLogin } from "../controllers/users.js";
-
-const router = express.Router();
-
-router.post("/auth", googleLogin);
-
-export default router;
