eeca77a267e5bd53998ddee7c26a6289e089c9e8
SantiCamm <santi.cammarano@gmail.com>
2022-02-18 17:06:10 -0300

    More bugs

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ import dotenv from "dotenv";
 import cors from "cors";
 import colors from "colors"
 import path from "path"
+import { dirname } from 'path';
 import todos from "./routes/todos.js";
 import users from "./routes/users.js";
 import folders from "./routes/folders.js";
+
 
 dotenv.config({ path: ".env" });
 const PORT = process.env.PORT || 5000;
@@ -17,18 +19,20 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+  app.use(morgan("dev"));
 }
 
 app.use("/folders", folders);
 app.use("/todos", todos);
 app.use("/users", users);
 
-if (process.env.NODE_ENV === "production") {
+const __dirname = dirname(__filename);
 
+if (process.env.NODE_ENV === "production") {
+  
   // We make our build folder the static folder
   app.use(express.static('client/build'));
-
+  
   // Each request (except our api routes) will load the index.html file
 app.get('*', (req, res) =>
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
